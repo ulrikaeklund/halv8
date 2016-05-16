fe71979be5659884058372f44ef2db674db2f28b
Ulrika <ulrikaeklund@Ulrikas-MacBook-Pro.local>
2016-05-16 22:40:04 +0200

    Lite mer layout, bla redigera profil

--- profil.php
+++ profil.php
@@ -8,5 +8,8 @@ include 'includes/overall/header.php';
     </div>
     <h2>Bror Bugge</h2>
     <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna ip sum dolore. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna ip sum dolore.</p>
+    <form method="post" id="editprof" action="register_profile.php">
+        <input type="submit" class="btn" name="redi_btn" value="Redigera din profil">
+    </form>
 </div>
 <?php include 'includes/overall/footer.php'?>

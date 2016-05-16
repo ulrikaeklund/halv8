2b28e339b3ccf6d08f712dc404b4dc99b539e801
eliasalmqvist <almqvist94@hotmail.com>
2016-05-16 10:34:19 +0200

    reg/login

--- core/init.php
+++ core/init.php
@@ -8,13 +8,9 @@ require 'functions/users.php';
 
 $errors = array();
 
-<<<<<<< HEAD
 if (logged_in() === true) {
     $session_user_id = $_SESSION['user_id'];
     $user_data = user_data($session_user_id, 'first_name', 'last_name', 'email', 'password');
 }
 
-=======
-function logged_in() {return false;}
->>>>>>> e9ada67902b802c886067680f8d5de3c024fecdd
 ?>

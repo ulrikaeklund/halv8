b6fe7e2c0025fb3b541d1b883921de5417bb4bd8
Brorbugge <fred@nl119-176-72.student.uu.se>
2016-05-18 08:29:06 +0200

    FAQ
    ASF

---
+++ FAQ.php
@@ -0,0 +1,30 @@
+ <?php
+include 'core/init.php';
+include 'includes/overall/header.php';
+?>
+<html>
+<div id="rubfr">
+<h1>Frågor</h1>
+</div>
+<section class="faq-section1">
+    <input type="checkbox" id="f1">
+    <label for="f1">Hur blir jag placerad i en bjudning?</label>           
+    <p> hmmm </p>
+</section>
+<section class="faq-section2">
+    <input type="checkbox" id="q2">
+    <label for="q2">Behöver jag lämna någon personlig information när jag registrerar mig hos halvåttahosmig?</label>     
+      <p>hmmm</p> 
+</section>
+<section class="faq-section3">
+    <input type="checkbox" id="q3">
+    <label for="q3">Hur lång tid tar det för mig att bli placerad i en bjudning?</label>           
+    <p>hmmm</p>  
+</section>
+<section class="faq-section4">
+    <input type="checkbox" id="q4">
+    <label for="q4">Hur tar jag kontakt med personerna i min bjudning?</label>         
+    <p>hmmm</p> 
+</section>
+<?php include 'includes/overall/footer.php'?>
+

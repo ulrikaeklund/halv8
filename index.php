f8dd7ce7449e78da5872a71ca15aa77df2b0ea68
eliasalmqvist <almqvist94@hotmail.com>
2016-05-24 21:01:11 +0200

    css uppdaterad

--- index.php
+++ index.php
@@ -5,16 +5,14 @@ if (logged_in()) {
     echo '<script type="text/javascript">
                 window.location = "bjudning.php"
             </script>'; 
-    exit();
-} else {
-    include 'login.php';
-}
-?>
+    exit(); 
+    }
+include 'login.php'; ?>
+
 <div id="omOss">
     <h2>Om oss </h2>
     <p> Inspirerade av TV4s matlagningsprogram “Halv åtta hos mig” är vår ambition att koppla ihop människor som bor i samma kommun att bjuda över varandra och äta middag hemma hos dem. En bjudningsgrupp bildas av fyra deltagare som var och en är ansvarig för att bjuda över de andra deltagarna på middag en dag. </p>
 </div>
-
 
 <?php include 'register.php';
 include 'includes/overall/footer.php';?>   
--- register.php
+++ register.php
@@ -79,35 +79,3 @@ if (empty($_POST['regBtn']) === false && empty($errors) === true) {
         </li>
     </ul>
 </form>
-
-
-
-
-
-<!--<form method="post" id="regForm" name="regForm" action="">
-    <h2>Registrera dig här</h2>
-    <input type="text" name="email" placeholder="E-mail">   
-        
-    <input type="password" name="password" placeholder="Lösenord">
-    <input type="password" name="password_again" placeholder="Upprepa lösenord">
-    <select name="city">
-        <option selected="true" style="display:none;" value="Välj stad">Välj stad</option>
-        <option value="Borås">Borås</option>
-        <option value="Eskilstuna">Eskilstuna</option>
-        <option value="Gävle">Gävle</option>
-        <option value="Göteborg">Göteborg</option>
-        <option value="Helsingborg">Helsingborg</option>
-        <option value="Jönköping">Jönköping</option>
-        <option value="Linköping">Linköping</option>
-        <option value="Lund">Lund</option>
-        <option value="Malmö">Malmö</option>
-        <option value="Norrköping">Norrköping</option>
-        <option value="Stockholm">Stockholm</option>
-        <option value="Umeå">Umeå</option>
-        <option value="Uppsala">Uppsala</option>
-        <option value="Västerås">Västerås</option>
-        <option value="Örebro">Örebro</option>
-    </select>
-    <input type="submit" class="btn" name="regBtn" value="Skapa konto">
-</form> -->
-

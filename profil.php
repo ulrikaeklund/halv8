21ba3bb1e890d223cbc7fbc807da3cd2a049e3a2
Ulrika <ulrikaeklund@Ulrikas-MacBook-Pro.local>
2016-05-29 23:49:22 +0200

    profilbild

--- profil.php
+++ profil.php
@@ -5,29 +5,54 @@ include 'includes/overall/header.php';
 
 <div id="profilContent">
     <div class="avatar">
-        <?php 
-        if(empty($user_data['profile']) === true){
-            echo '<img src="img/pictures/defaultprofile.png" alt="Default profilbild" id="default">';
+        <?php
+        if(isset($_FILES['profile']) === true){
+            if(empty($_FILES['profile']['name']) === true){
+                echo 'Vänligen välj en fil!';
+            }
+            else{
+                $allowed = array('jpg', 'jpeg', 'gif', 'png');
+                
+                $file_name = $_FILES['profile']['name'];
+                $file_name = explode('.', $file_name);
+                $file_extn = strtolower(end($file_name ));
+                $file_temp = $_FILES['profile']['tmp_name'];
+                
+                if(in_array($file_extn, $allowed) === true){
+                    change_profile_image($session_user_id, $file_temp, $file_extn);
+                    
+                }
+                else{
+                    echo 'Felaktig filtyp. Tillåtna filtyper: ';
+                    echo implode(', ', $allowed);
+                }
+            }
         }
-         ?>
+        
+        if(empty($user_data['profile']) === false){
+            echo '<img src="', $user_data['profile'], '" alt="', $user_data['first_name'], ' \'s profilbild" id="profilbild">';   
+        }
+        ?>
     </div>
     <div id="profil">
         <form action="" method="post" enctype="multipart/form-data" id="avatarPhoto">
-            <input name="profile" type="file" id="profile" style="display: none;">
-            <img src="img/fotocamera.png" class="uploadPicture" onclick="document.getElementById('profile').click();" title="Välj en profilbild">
-            <script>    
-                alert("Hello\nHow are you?");
-                var fileToRead = document.getElementById("profile");
-                fileToRead.addEventListener("change", function(event) {
-                    
-            }
-            </script>
+<!--
+            <label for="profile">
+                <img src="img/fotocamera.png" class="uploadPicture" title="Välj en profilbild">
+style="display: none;
+            </label>
+-->
+            <input name="profile" type="file" id="profile">
+            <input type="submit" class="btn" value="Välj bild">
+            
         </form>
     </div>
-    <?php echo '<h2>' . $user_data['first_name'] . ' ' . $user_data['last_name'] . '</h2>';
-    echo '<p>' . $user_data['about'] .'</p>';?>
+    <?php 
+    echo '<h2>' . $user_data['first_name'] . ' ' . $user_data['last_name'] . '</h2>';
+    echo '<p>' . $user_data['about'] .'</p>';
+    ?>
     <form method="post" id="editprof" action="register_profile.php">
         <input type="submit" class="btn" name="redi_btn" value="Redigera profil">
     </form>
 </div>
-<?php include 'includes/overall/footer.php'?>
+<?php include 'includes/overall/footer.php'; ?>
